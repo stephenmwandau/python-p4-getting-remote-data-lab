347c407cb9bdfc98003dd8247c27e3e2fa103cf7
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-10-28 15:47:30 +0300

    fetching and decoding response

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response_body = requests.get(self.url).content
+        return response_body
 
     def load_json(self):
-        pass
+        byte_response = self.get_response_body()
+        byte_response_string = byte_response.decode('utf-8')
+        json_response = json.loads(byte_response_string)
+        print(f'{json_response}')
+        return json_response
+
+request1 = GetRequester(url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+
+request1.load_json()
